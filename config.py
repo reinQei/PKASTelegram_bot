85dc2fd9c5c35225d3589074437049f36630f05e
reinQei <reinqler@gmail.com>
2023-09-01 14:53:54 +0300

    lock ids

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
+from venv.tokens import API_TOKENs, id_admins
 
-API_TOKEN = '5960192545:AAGRhc6fm1whqI0wbnhfSC2PYzZ-3u9vfTI'
+API_TOKEN = API_TOKENs
 
 # ------ чат id администраторов ------ #
-admins = '1039351284'
+admins = id_admins
 
-
-# ------ ключ от бота для тестов ------ #
-'5960192545:AAGRhc6fm1whqI0wbnhfSC2PYzZ-3u9vfTI'
-# ------ ключ от бота для общего доступа ------ #
-'5663228436:AAFE9hzXYe7ZEy2dhkxrLcpP-G86EM4XcEU'
